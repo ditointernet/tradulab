8f2b128234c40b75433d4485f74f4677eb6728b0
Uriell <uriellarttusviana+github@gmail.com>
2020-11-10 19:08:54 -0300

    we tested everything and it seems ok, including a project fix

--- packages/api/src/modules/role/types.ts
+++ packages/api/src/modules/role/types.ts
@@ -63,6 +63,7 @@ export default gql`
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 # Por que esses diferentes tipos de Role... ?
 >>>>>>> Feita lógica de restrição de convites de cargos no módulo role
@@ -72,6 +73,9 @@ export default gql`
 =======
 # Por que esses diferentes tipos de Role... ?
 >>>>>>> Feita lógica de restrição de convites de cargos no módulo role
+=======
+
+>>>>>>> we tested everything and it seems ok, including a project fix
   type RoleWithProject {
     id: ID!
     role: String!
