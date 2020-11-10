737f9e6d4e0749d9e7628fa37b195eb5c88b7b59
Uriell <uriellarttusviana+github@gmail.com>
2020-11-10 19:08:54 -0300

    we tested everything and it seems ok, including a project fix

--- packages/api/src/modules/project/types.ts
+++ packages/api/src/modules/project/types.ts
@@ -10,14 +10,34 @@ export default gql`
     createdAt: Date!
     updatedAt: Date!
   }
+<<<<<<< HEAD
   input createProjectPayload {
     name: String!
     private: Boolean
+=======
+
+  type ProjectWithUser {
+    id: ID!
+    slug: String!
+    displayName: String!
+    owner: User!
+    private: Boolean!
+    createdAt: Date!
+    updatedAt: Date!
+  }
+
+  extend type Query {
+    myProjects: [RoleWithProject!]!
+>>>>>>> we tested everything and it seems ok, including a project fix
   }
   extend type Mutation {
+<<<<<<< HEAD
     createProject(payload: createProjectPayload): Project!
   }
   extend type Query {
     listProjects: [RoleWithProject!]!
+=======
+    createProject(displayName: String!, private: Boolean): ProjectWithUser!
+>>>>>>> we tested everything and it seems ok, including a project fix
   }
 `;
