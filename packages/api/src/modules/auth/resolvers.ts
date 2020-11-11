ccd2876ff11a5467b64b8f60098603f5558224d0
Bolivar <webdevelopmentbolivar@gmail.com>
2020-11-11 15:26:11 -0300

    pull

--- packages/api/src/modules/auth/resolvers.ts
+++ packages/api/src/modules/auth/resolvers.ts
@@ -195,12 +195,16 @@ async function login(_parent, { payload: { email, password } }) {
 }
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 // Não acho legal este nomes e esta maneira de exportar
 =======
 
 >>>>>>> erase comments
+=======
+// Não acho legal este nomes e esta maneira de exportar
+>>>>>>> pull
 export const queries = { login };
 >>>>>>> pull
 export const mutations = { createUser };
