a69d604f92a9b21d9671a3c4655f3ad58c5a27db
Julio Cezar <jctaraujo@hotmail.com>
2020-11-12 19:12:36 -0300

    Create file resolver working at front-end and back-end without error treatment

--- packages/api/src/modules/file/types.ts
+++ packages/api/src/modules/file/types.ts
@@ -15,6 +15,7 @@ export default gql`
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   scalar FileUpload
   enum Extentions {
     csv
@@ -113,6 +114,8 @@ export default gql`
 =======
 =======
 >>>>>>> Criado o module files e a resolver create File
+=======
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
   scalar FileUpload
 
 >>>>>>> Create file resolver working at front-end and back-end without error treatment
@@ -171,16 +174,29 @@ export default gql`
 >>>>>>> Create file resolver working at front-end and back-end without error treatment
 =======
 =======
+=======
+  scalar FileUpload
+
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
   type File {
     id: ID!
-    key: String!
     filename: String!
+<<<<<<< HEAD
     translation_progress: Int!
     approval_progress: Int!
     source_language: String!
     extesion: String!
 >>>>>>> Criado o module files e a resolver create File
+<<<<<<< HEAD
 >>>>>>> Criado o module files e a resolver create File
+=======
+=======
+    translationProgress: Int!
+    approvalProgress: Int!
+    sourceLanguage: String!
+    extension: String!
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
     project: Project!
     createdAt: Date!
     updatedAt: Date!
@@ -188,6 +204,7 @@ export default gql`
 
   extend type Mutation {
     createFile(
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -289,6 +306,8 @@ export default gql`
 =======
 =======
 >>>>>>> Criado o module files e a resolver create File
+=======
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
       file: FileUpload!
 <<<<<<< HEAD
 >>>>>>> Create file resolver working at front-end and back-end without error treatment
@@ -304,7 +323,13 @@ export default gql`
       extesion: String!, 
       source_language: String!
 >>>>>>> Criado o module files e a resolver create File
+<<<<<<< HEAD
 >>>>>>> Criado o module files e a resolver create File
+=======
+=======
+      file: FileUpload!
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
     ): File!
   }
 >>>>>>> Criado o module files e a resolver create File
--- packages/web/src/UploadForm.tsx
+++ packages/web/src/UploadForm.tsx
@@ -9,6 +9,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react';
 import { useMutation, gql, useQuery } from '@apollo/client';
 =======
@@ -36,12 +37,19 @@ import { useMutation, gql, useQuery } from '@apollo/client';
 >>>>>>> Create file resolver working at front-end and back-end without error treatment
 >>>>>>> Create file resolver working at front-end and back-end without error treatment
 =======
+=======
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
 <<<<<<< HEAD
 =======
 >>>>>>> Create file resolver working at front-end and back-end without error treatment
 =======
 >>>>>>> Corrigido erro de cors pra qualquer request
+<<<<<<< HEAD
 >>>>>>> Corrigido erro de cors pra qualquer request
+=======
+=======
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
 import React from "react";
 import { useMutation, gql, useQuery } from "@apollo/client";
 >>>>>>> Create file resolver working at front-end and back-end without error treatment
@@ -296,6 +304,7 @@ export default function UploadForm() {
     const sourceLanguage = "PT-BR";
     if (!file) return;
     createFile({ variables: { file, projectId, sourceLanguage } });
+<<<<<<< HEAD
 >>>>>>> Corrigido erro de cors pra qualquer request
 =======
 import React from 'react';
@@ -438,11 +447,52 @@ export default function UploadForm() {
     if (!file) return;
     createFile({ variables: { file, projectId, sourceLanguage } });
 >>>>>>> Corrigido erro de cors pra qualquer request
+=======
+=======
+import React from 'react';
+import { useMutation, gql, useQuery } from '@apollo/client';
+
+// const LOGIN = gql`
+//   query login ($email: String!, $password: String!){
+//     login (
+//       email: $email
+//       password: $password
+//     ) {
+//       token
+//     }
+//   }
+// `
+
+const UPLOAD_FILE = gql`
+  mutation createFile($file: Upload!){
+    createFile(file: $file){
+      filename
+    }
+  }
+`
+export default function UploadForm() {
+  // const { data, refetch } = useQuery(LOGIN, {
+  //   variables: { email: 'julinho2801@gmail.com', password: '123456' }
+  // });
+
+  // if (data) console.log(data.login.token)
+
+  const [createFile, { data }] = useMutation(UPLOAD_FILE);
+
+  console.log(data)
+  
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files![0]
+    if (!file) return
+    createFile({ variables: { file } })
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
   };
 
   return (
     <div>
       <h1>Upload File</h1>
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -534,5 +584,15 @@ export default function UploadForm() {
 }
 >>>>>>> formatting changes and some typings
 =======
+=======
+      <input type="file" onChange={handleFileChange} />
+    </div>
+  );
+=======
+      <input type='file' onChange={handleFileChange} />
+    </div>
+  )
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
+>>>>>>> Create file resolver working at front-end and back-end without error treatment
 };
 >>>>>>> Add Apollo Erros, fix merge conflicts, removing comments
