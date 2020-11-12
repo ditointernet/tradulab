5cfafd70de15c95fd385cb3458d96e8db60c70c1
Julio Cezar <jctaraujo@hotmail.com>
2020-11-12 19:12:36 -0300

    Create file resolver working at front-end and back-end without error treatment

--- packages/api/src/modules/file/resolvers.ts
+++ packages/api/src/modules/file/resolvers.ts
@@ -1,29 +1,34 @@
+import path from 'path';
+import fs from 'fs';
 import { model as File } from '.'
 import { model as Project } from '../project';
 
-async function createFile(parent, args) {
-  const project = await Project.findOne({ project: args.project })
+async function createFile(parent, args, context) {
+  console.log(args.file);
+  const { filename, mimetype, encoding } = await args.file;
 
-  if (!project) {
-    throw new Error('The provided project does not exist.');
-  }
+  // const project = await Project.findOne({ project: args.project })
 
-  const file = new File({
-    filename: args.filename,
-    translation_progress: 0,
-    approval_progress: 0,
-    source_language: args.source_language,
-    extension: args.extension,
-    project,
-  });
+  // if (!project) {
+  //   throw new Error('The provided project does not exist.');
+  // }
 
-  try {
-    await file.save();
-  } catch (err) {
-    throw err;
-  }
+  // const file = new File({
+  //   filename,
+  //   translation_progress: 0,
+  //   approval_progress: 0,
+  //   source_language: args.source_language,
+  //   extension: filename.split('.').pop(),
+  //   project,
+  // });
 
-  return file;
+  // try {
+  //   await file.save();
+  // } catch (err) {
+  //   throw err;
+  // }
+
+  // return file;
 }
 
 export const mutations = { createFile };
