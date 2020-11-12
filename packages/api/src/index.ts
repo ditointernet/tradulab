737c615074ab07f499057f86debdb3dd004e194d
Bolivar <webdevelopmentbolivar@gmail.com>
2020-11-12 16:48:23 -0300

    Update Role

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,14 +4,34 @@ import { env } from './helpers';
 import * as middlewares from './middlewares';
 
 const app = express();
+<<<<<<< HEAD
 
+=======
+<<<<<<< HEAD
+=======
+// declarando uma nova variável
+>>>>>>> Update Role
+>>>>>>> Update Role
 app.locals.mongo = mongo;
 
 app.use(middlewares.jwt);
 
 middlewares.apollo(app);
 
+<<<<<<< HEAD
 app.use(middlewares.error);
+=======
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    return res.status(401).json({ error: 'JWT Expired.' });
+  }
+<<<<<<< HEAD
+  console.error('Error: ', err.name);
+=======
+  console.log("Error: ", err.name)
+>>>>>>> Update Role
+});
+>>>>>>> Update Role
 
 function start() {
   const EXPRESS_PORT = env.getOrThrow('EXPRESS_PORT');
---
+++ packages/api/src/middlewares/apollo.ts
@@ -0,0 +1,141 @@
+import { GraphQLDateTime } from 'graphql-iso-date';
+import { ApolloError, ApolloServer, AuthenticationError, ForbiddenError, gql } from 'apollo-server-express';
+import { buildFederatedSchema } from '@apollo/federation';
+import { applyMiddleware } from 'graphql-middleware';
+import { not, and, rule, shield } from 'graphql-shield';
+
+import { auth, user, project, role } from '../modules';
+import { ROLES } from '../modules/role/constants';
+
+// A GraphQL service is created by defining types and fields on those types, then providing funcions for each field on each type
+// Create e object types;
+// Custon scalar types
+const typeDefs = gql`
+  scalar Date
+
+  type Query {
+    _: Boolean
+  }
+
+  type Mutation {
+    _: Boolean
+  }
+
+  type Subscription {
+    _: Boolean
+  }
+
+  ${auth.types}
+  ${user.types}
+  ${project.types}
+  ${role.types}
+`;
+
+const isAuthenticated = rule()((parent, args, { user }) => {
+  if (!user) {
+    return new AuthenticationError('You must be logged in.');
+  }
+  return true;
+});
+
+const isManagerOrOwner = rule()(
+  async (parent, { projectId }, { user: { id: currentUserId } }) => {
+    try {
+      const projectRole = await role.model.findOne({
+        project: projectId,
+        user: currentUserId,
+      });
+
+      if (projectRole && [ROLES.MANAGER, ROLES.OWNER].includes(projectRole.role)) {
+        return true;
+      }
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+
+    return new ForbiddenError('You must be owner or manager in this project.');
+  }
+);
+
+export default function ApolloMiddleware(app) {
+  const apolloServer = new ApolloServer({
+    schema: applyMiddleware(
+      buildFederatedSchema([
+        {
+          typeDefs,
+          resolvers: {
+            Date: GraphQLDateTime,
+            Query: {
+              ...auth.resolvers.queries,
+              ...user.resolvers.queries,
+              ...project.resolvers.queries,
+              ...role.resolvers.queries,
+            },
+            Mutation: {
+              ...auth.resolvers.mutations,
+              ...project.resolvers.mutations,
+              ...role.resolvers.mutations,
+            },
+          },
+        },
+      ]),
+      shield(
+        {
+          Query: {
+            login: not(isAuthenticated, new ApolloError('Someone is already logged in.', 'ALREADY_LOGGED_IN')),
+            me: isAuthenticated,
+            myProjects: isAuthenticated,
+          },
+          Mutation: {
+            createUser: not(isAuthenticated),
+            createProject: isAuthenticated,
+            inviteUserToProject: and(
+              isAuthenticated,
+              isManagerOrOwner
+              // isNotTargetingHigherRoles
+            ),
+            removeUserFromProject: and(
+              isAuthenticated,
+              isManagerOrOwner
+              // isNotTargetingHigherRoles
+            ),
+            updateUserProjectRole: and(
+              isAuthenticated,
+              isManagerOrOwner
+              // isNotTargetingHigherRoles
+            ),
+          },
+        },
+        {
+          // https://github.com/maticzav/graphql-shield
+          fallbackError: (err, parent, args, context, info) => {
+            if (err instanceof ApolloError) {
+              // expected errors
+              return err;
+            } else if (err instanceof Error) {
+              // unexpected errors
+              console.error(err);
+              return new ApolloError('Internal server error', 'ERR_INTERNAL_SERVER');
+            } else {
+              // what the hell got thrown
+              console.error('The resolver threw something that is not an error.');
+              console.error(err);
+              return new ApolloError('Internal server error', 'ERR_INTERNAL_SERVER');
+            }
+          },
+          allowExternalErrors: true,
+        }
+      )
+    ),
+    context: async ({ req: { auth } }: any) => {
+      if (typeof auth === 'object' && auth.id) {
+        return { user: await user.model.findById(auth.id) };
+      }
+
+      return {};
+    },
+  });
+
+  apolloServer.applyMiddleware({ app });
+}
