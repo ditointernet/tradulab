a5291f15a89ad0442d8e1f569562cee87a8004b0
Uriell <uriellarttusviana+github@gmail.com>
2020-11-12 18:37:51 -0300

    removing comments

--- packages/api/src/middlewares/jwt.ts
+++ packages/api/src/middlewares/jwt.ts
@@ -26,6 +26,7 @@ export default async function jwtMiddleware(
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   const isAuthorized = bearerRegex.test(authorization);
 
   if (isAuthorized) {
@@ -55,6 +56,9 @@ export default async function jwtMiddleware(
 =======
   // testa de se o Bearer no Header
 >>>>>>> Update Role
+=======
+
+>>>>>>> removing comments
   if (bearerRegex.test(authorization)) {
 >>>>>>> Update Role
     const token = authorization.split(bearerRegex)[1];
