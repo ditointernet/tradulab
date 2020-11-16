9c1bfbae4e06e8860baaff3909a36ff5969e9a23
Julio Cezar <jctaraujo@hotmail.com>
2020-11-16 19:04:39 -0300

    Corrigido erro de cors pra qualquer request

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,8 +2,14 @@ import * as express from 'express';
 import { mongo } from './config';
 import * as middlewares from './middlewares';
 import { env } from './helpers';
+import { graphqlUploadExpress } from 'graphql-upload';
 
 const app = express();
+<<<<<<< HEAD
+=======
+
+// app.use(graphqlUploadExpress({ maxFileSize: 100, maxFiles: 1 }));
+>>>>>>> Corrigido erro de cors pra qualquer request
 app.locals.mongo = mongo;
 
 app.use(middlewares.jwt);
@@ -11,6 +17,7 @@ app.use(middlewares.jwt);
 middlewares.apollo(app);
 
 app.use(function (err, req, res, next) {
+  console.log('ERROOOO', err)
   if (err.name === 'UnauthorizedError') {
     return res.status(401).json({ error: 'JWT Expired.' });
   }
