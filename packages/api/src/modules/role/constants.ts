3628c9ea9470f2da255763a19a7d5ea3577749ac
Bolivar <webdevelopmentbolivar@gmail.com>
2020-11-17 17:23:23 -0300

    remove comments

--- packages/api/src/modules/role/constants.ts
+++ packages/api/src/modules/role/constants.ts
@@ -134,6 +134,7 @@ export const ROLES_LIST = [
 };
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 export const ROLES_LIST = [ // mudar nome
 >>>>>>> Feita lógica de restrição de convites de cargos no módulo role
 =======
@@ -142,6 +143,9 @@ export const ROLES_LIST = [ // mudar nome
 >>>>>>> Feita lógica de restrição de convites de cargos no módulo role
 export const ROLES_LIST = [
 >>>>>>> erase comments
+=======
+export const ROLES_LIST = [ // mudar nome
+>>>>>>> remove comments
   ROLES.OWNER,
   ROLES.MANAGER,
   ROLES.DEVELOPER,
