0e77c1fd86fb630574a3aca280025d852b3cc453
Uriell <uriellarttusviana+github@gmail.com>
2020-11-17 18:33:23 -0300

    file size limit from content length header

--- packages/api/src/modules/file/resolvers.ts
+++ packages/api/src/modules/file/resolvers.ts
@@ -46,9 +46,12 @@ import {
 import { model as File } from '.';
 import { model as Project } from '../project';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { model as Role } from '../role';
 import TradulabError from '../../errors';
 =======
+=======
+>>>>>>> file size limit from content length header
 <<<<<<< HEAD
 import { ERROR_CODES as fileCodes } from './constants';
 import { ERROR_CODES as projectCodes } from '../project/constants';
@@ -114,6 +117,9 @@ import { FileUpload } from 'graphql-upload';
 import { model as File } from '.';
 >>>>>>> formatting changes and some typings
 import { model as Project } from '../project';
+=======
+import { MAX_ALLOWED_FILE_SIZE_IN_BYTES } from './constants';
+>>>>>>> file size limit from content length header
 
 interface ICreateFileArgs {
   file: FileUpload;
@@ -124,8 +130,13 @@ interface ICreateFileArgs {
 async function createFile(parent, args: ICreateFileArgs, context) {
   const { createReadStream, filename } = await args.file;
 
+  if (context.contentLength > MAX_ALLOWED_FILE_SIZE_IN_BYTES) {
+    throw new Error('File size exceeded, limit is 5MB.');
+  }
+
   // const stream = createReadStream();
   // stream.on('data', (chunk) => console.log(chunk.toString()));
+
   const project = await Project.findOne({ _id: args.projectId });
 
 <<<<<<< HEAD
