5c2c1d20f76c1ff158d8b5f8db954f3cf394e627
Uriell <uriellarttusviana+github@gmail.com>
2020-11-17 17:53:13 -0300

    formatting changes and some typings

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -10,6 +10,7 @@ import { env } from './helpers';
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import * as middlewares from './middlewares';
 <<<<<<< HEAD
@@ -52,6 +53,9 @@ const cors = require('cors');
 >>>>>>> Corrigido erro de cors pra qualquer request
 =======
 >>>>>>> Unnecessary comments and logs removed, add ApolloErrors
+// import { graphqlUploadExpress } from 'graphql-upload';
+>>>>>>> formatting changes and some typings
+=======
 // import { graphqlUploadExpress } from 'graphql-upload';
 >>>>>>> formatting changes and some typings
 
@@ -176,6 +180,7 @@ app.use(function (err, req, res, next) {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   console.log('ERROOOO', err);
 =======
   console.log('ERROOOO', err)
@@ -213,6 +218,9 @@ app.use(function (err, _req, res, _next) {
 =======
   console.log('ERROOOO', err)
 >>>>>>> Corrigido erro de cors pra qualquer request
+=======
+  console.log('ERROOOO', err);
+>>>>>>> formatting changes and some typings
   if (err.name === 'UnauthorizedError') {
     return res.status(401).json({ error: 'JWT Expired.' });
   }
