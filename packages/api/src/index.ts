548e6b01d1453a9e0ec8fdbfc9868592364dcf56
Uriell <uriellarttusviana+github@gmail.com>
2020-11-17 17:53:13 -0300

    formatting changes and some typings

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -8,6 +8,7 @@ import { env } from './helpers';
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import * as middlewares from './middlewares';
 
 const app = express();
@@ -38,6 +39,8 @@ import { graphqlUploadExpress } from 'graphql-upload';
 import { graphqlUploadExpress } from 'graphql-upload';
 >>>>>>> Corrigido erro de cors pra qualquer request
 =======
+=======
+>>>>>>> formatting changes and some typings
 // import { graphqlUploadExpress } from 'graphql-upload';
 >>>>>>> formatting changes and some typings
 =======
@@ -63,6 +66,9 @@ const app = express();
 >>>>>>> Corrigido erro de cors pra qualquer request
 =======
 import { graphqlUploadExpress } from 'graphql-upload';
+=======
+// import { graphqlUploadExpress } from 'graphql-upload';
+>>>>>>> formatting changes and some typings
 
 const app = express();
 <<<<<<< HEAD
@@ -96,6 +102,7 @@ app.use(function (err, req, res, next) {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> Corrigido erro de cors pra qualquer request
   console.log('ERROOOO', err);
@@ -104,6 +111,8 @@ app.use(function (err, req, res, next) {
 >>>>>>> Corrigido erro de cors pra qualquer request
 <<<<<<< HEAD
 =======
+=======
+>>>>>>> formatting changes and some typings
   console.log('ERROOOO', err);
 >>>>>>> formatting changes and some typings
 =======
@@ -114,8 +123,11 @@ app.use(function (err, req, res, next) {
 =======
   console.log('ERROOOO', err);
 >>>>>>> formatting changes and some typings
+<<<<<<< HEAD
 =======
 >>>>>>> Corrigido erro de cors pra qualquer request
+=======
+>>>>>>> formatting changes and some typings
   if (err.name === 'UnauthorizedError') {
     return res.status(401).json({ error: 'JWT Expired.' });
   }
