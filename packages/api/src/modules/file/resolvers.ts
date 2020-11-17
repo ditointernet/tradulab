e9fd55cfe66e6e0cf3697a83b3d7ee7361ee6f5b
Uriell <uriellarttusviana+github@gmail.com>
2020-11-17 17:53:13 -0300

    formatting changes and some typings

--- packages/api/src/modules/file/resolvers.ts
+++ packages/api/src/modules/file/resolvers.ts
@@ -31,6 +31,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 =======
 >>>>>>> Criado o module files e a resolver create File
@@ -40,6 +41,8 @@
 >>>>>>> file size limit from content length header
 =======
 >>>>>>> Criado o module files e a resolver create File
+=======
+>>>>>>> formatting changes and some typings
 import { ApolloError } from 'apollo-server-express';
 >>>>>>> Add Apollo Erros, fix merge conflicts, removing comments
 =======
@@ -664,16 +667,25 @@ export const queries = { listFiles };
 
 =======
 import { model as File } from '.'
+=======
+import { FileUpload } from 'graphql-upload';
+
+import { model as File } from '.';
+>>>>>>> formatting changes and some typings
 import { model as Project } from '../project';
-import { model as Role } from '../role';
 
-async function createFile(parent, args, context) {
+interface ICreateFileArgs {
+  file: FileUpload;
+  projectId: string;
+  sourceLanguage: string;
+}
+
+async function createFile(parent, args: ICreateFileArgs, context) {
   const { createReadStream, filename } = await args.file;
-  console.log(args.file);
 
-  const stream = createReadStream();
-  stream.on('data', (chunk) => console.log(chunk.toString()));
-  const project = await Project.findOne({ _id: args.projectId })
+  // const stream = createReadStream();
+  // stream.on('data', (chunk) => console.log(chunk.toString()));
+  const project = await Project.findOne({ _id: args.projectId });
 
 <<<<<<< HEAD
 <<<<<<< HEAD
