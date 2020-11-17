3e64e1a26ede8ef6351f6e15165c485b8e25bb58
Uriell <uriellarttusviana+github@gmail.com>
2020-11-17 18:33:23 -0300

    file size limit from content length header

--- packages/api/src/middlewares/apollo.ts
+++ packages/api/src/middlewares/apollo.ts
@@ -1,5 +1,4 @@
 import { GraphQLDateTime } from 'graphql-iso-date';
-<<<<<<< HEAD
 import {
   ApolloError,
   ApolloServer,
@@ -12,13 +11,6 @@ import { buildFederatedSchema } from '@apollo/federation';
 import { applyMiddleware } from 'graphql-middleware';
 import { not, and, or, rule, shield } from 'graphql-shield';
 import cors from 'cors';
-=======
-import { ApolloServer, gql, GraphQLUpload } from 'apollo-server-express';
-import { buildFederatedSchema } from '@apollo/federation';
-import { applyMiddleware } from 'graphql-middleware';
-import { not, and, or, rule, shield } from 'graphql-shield';
-import cors from "cors";
->>>>>>> Create file resolver working at front-end and back-end without error treatment
 
 import { auth, user, project, role, file } from '../modules';
 import { ROLES } from '../modules/role/constants';
@@ -26,15 +18,7 @@ import { ROLES } from '../modules/role/constants';
 const corsOptions: cors.CorsOptions = {
   origin: 'http://localhost:3000',
   credentials: true,
-<<<<<<< HEAD
-<<<<<<< HEAD
   allowedHeaders: ['Authorization', 'content-type'],
-=======
-  allowedHeaders: 'Authorization',  
->>>>>>> Create file resolver working at front-end and back-end without error treatment
-=======
-  allowedHeaders: ['Authorization', 'content-type'],
->>>>>>> Corrigido erro de cors pra qualquer request
 };
 
 const typeDefs = gql`
@@ -85,39 +69,11 @@ const isOneOfTheseRoles = (allowedRoles: string[]) =>
     );
   });
 
-<<<<<<< HEAD
 const isManagerOrOwner = isOneOfTheseRoles([ROLES.OWNER, ROLES.MANAGER]);
 const isDeveloper = isOneOfTheseRoles([ROLES.DEVELOPER]);
-=======
-    return false;
-  }
-);
-const isDeveloper = rule()(
-  async (parent, { projectId }, { user: { id: currentUserId } }) => {
-    if (user) {
-      try {
-        const projectRole = await role.model.findOne({
-          project: projectId,
-          user: currentUserId,
-        });
-        if (projectRole.role === ROLES.DEVELOPER)
-          return true;
-      } catch (err) {
-        console.error(err);
-        return false;
-      }
-    }
-
-    return false;
-  }
-);
->>>>>>> Corrigido erro de cors pra qualquer request
 
 export default function ApolloMiddleware(app) {
   const apolloServer = new ApolloServer({
-    uploads: {
-      maxFileSize: 200,
-    },
     schema: applyMiddleware(
       buildFederatedSchema([
         {
@@ -150,18 +106,7 @@ export default function ApolloMiddleware(app) {
           Mutation: {
             createUser: not(isAuthenticated),
             createProject: isAuthenticated,
-<<<<<<< HEAD
-<<<<<<< HEAD
             createFile: and(isAuthenticated, or(isDeveloper, isManagerOrOwner)),
-=======
-            createFile: isAuthenticated,
->>>>>>> Criado o module files e a resolver create File
-=======
-            createFile: and(
-              isAuthenticated,
-              or(isDeveloper, isManagerOrOwner)
-            ),
->>>>>>> Corrigido erro de cors pra qualquer request
             inviteUserToProject: and(
               isAuthenticated,
               isManagerOrOwner
--- packages/api/src/modules/file/constants.ts
+++ packages/api/src/modules/file/constants.ts
@@ -1,20 +1,2 @@
-<<<<<<< HEAD
 export const EXTENSION_LIST = ['json', 'txt', 'csv'];
 export const MAX_ALLOWED_FILE_SIZE_IN_BYTES = 1e3 * 1e3 * 5; // currently 5MB
-=======
-export const EXTENSION_LIST = [
-<<<<<<< HEAD
-  '.txt',
-  '.csv',
-  '.json',
-];
->>>>>>> Criado o module files e a resolver create File
-=======
-  'json',
-<<<<<<< HEAD
-];
->>>>>>> Create file resolver working at front-end and back-end without error treatment
-=======
-  'txt',
-];
->>>>>>> Corrigido erro de cors pra qualquer request
