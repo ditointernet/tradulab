e2b6c0baa56e2051c511f2ade50d762b34fbb1bc
Uriell <uriellarttusviana+github@gmail.com>
2020-11-17 17:53:13 -0300

    formatting changes and some typings

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from "react";
 import "./App.css";
 
@@ -91,50 +92,36 @@ const client = new ApolloClient({
   link: authLink.concat(uploadLink),
   cache: new InMemoryCache(),
 });
+=======
+import React from "react";
+import "./App.css";
+>>>>>>> formatting changes and some typings
 
-import { ApolloProvider, ApolloClient, InMemoryCache, ApolloLink, createHttpLink } from '@apollo/client';
-import { createUploadLink } from 'apollo-upload-client';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  ApolloLink,
+} from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
 
-import UploadForm, { LOGIN, UPLOAD_FILE } from './UploadForm';
-
-const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
-  credentials: 'include'
-});
-
-const token = ''
-
-// const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYWFmOWQ2YTBiODk2NTcwMjIyNTU1OCIsImlhdCI6MTYwNTU2MDUxNiwiZXhwIjoxNjA1NTYyMzE2fQ.n1LJEVLnaaKMrAyC4Xe6MG-TiLl_LdzeTop4ksFziqA';
+import UploadForm from "./UploadForm";
 
 const uploadLink = createUploadLink({
-  uri: 'http://localhost:3001/graphql',
-  credentials: 'include',
+  uri: "http://localhost:3001/graphql",
+  credentials: "include",
 });
-
-// const authLink = setContext((_, { headers }) => {
-//   // get the authentication token from local storage if it exists
-//   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYWFmOWQ2YTBiODk2NTcwMjIyNTU1OCIsImlhdCI6MTYwNTE4NzA5NiwiZXhwIjoxNjA1MTg4ODk2fQ.I3M_qSK97WWPdlayaM4_5InAmoJwcLFIH686iKXWnXs';
-//   // return the headers to the context so httpLink can read them
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : "",
-//     }
-//   }
-// });
-
-// const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYWFmOWQ2YTBiODk2NTcwMjIyNTU1OCIsImlhdCI6MTYwNTI3MjkyMywiZXhwIjoxNjA1Mjc0NzIzfQ.q544NyZ8ZS1K3n6edDIeIZo4vobIQG4a1pdXZZZExjU';
 
 const authLink = new ApolloLink((operation, forward) => {
   // Retrieve the authorization token from local storage.
-  // console.log(operation);
-  const token = localStorage.getItem('token');
-  if (!['login', 'createUser'].includes(operation.operationName)) {
+  const token = localStorage.getItem("token");
+
+  if (!["login", "createUser"].includes(operation.operationName)) {
     // Use the setContext method to set the HTTP headers.
     operation.setContext({
       headers: {
-        authorization: token ? `Bearer ${token}` : '',
-      }
+        authorization: token ? `Bearer ${token}` : "",
+      },
     });
   }
 
@@ -147,6 +134,7 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 =======
@@ -226,6 +214,8 @@ const client = new ApolloClient({
 >>>>>>> formatting changes and some typings
 =======
 >>>>>>> Corrigido erro de cors pra qualquer request
+=======
+>>>>>>> formatting changes and some typings
 function App() {
   return (
     <ApolloProvider client={client}>
