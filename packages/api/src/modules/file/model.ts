deb543fd6664baaa6ee93b5e27d17576194060a5
Uriell <uriellarttusviana+github@gmail.com>
2020-11-17 18:33:23 -0300

    file size limit from content length header

--- packages/api/src/modules/file/model.ts
+++ packages/api/src/modules/file/model.ts
@@ -18,6 +18,7 @@ import * as mongoose from 'mongoose';
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 =======
 >>>>>>> Criado o module files e a resolver create File
@@ -44,6 +45,8 @@ import { IProject } from '../project/model';
 import { EXTENSION_LIST } from './constants';
 =======
 >>>>>>> Rebase and resolving conflicts with master
+=======
+>>>>>>> file size limit from content length header
 import { IProject } from '../project/model';
 =======
 =======
@@ -94,6 +97,9 @@ import { EXTENSION_LIST } from './constants';
 =======
 >>>>>>> Criado o module files e a resolver create File
 >>>>>>> Criado o module files e a resolver create File
+=======
+
+>>>>>>> file size limit from content length header
 import { EXTENSION_LIST } from './constants';
 >>>>>>> Criado o module files e a resolver create File
 =======
@@ -547,6 +553,7 @@ export default model;
 =======
     filePath: {
       type: String,
+<<<<<<< HEAD
     },
 >>>>>>> Create file resolver working at front-end and back-end without error treatment
 =======
@@ -555,6 +562,14 @@ export default model;
 =======
     filePath: {
       type: String,
+=======
+<<<<<<< HEAD
+      required: true
+>>>>>>> Criado o module files e a resolver create File
+=======
+      required: true,
+>>>>>>> file size limit from content length header
+>>>>>>> file size limit from content length header
     },
 >>>>>>> Create file resolver working at front-end and back-end without error treatment
 =======
@@ -816,5 +831,11 @@ export default model;
 const model = mongoose.model('file', schema);
 
 export default model;
+<<<<<<< HEAD
 >>>>>>> Criado o module files e a resolver create File
+<<<<<<< HEAD
 >>>>>>> Criado o module files e a resolver create File
+=======
+=======
+>>>>>>> file size limit from content length header
+>>>>>>> file size limit from content length header
