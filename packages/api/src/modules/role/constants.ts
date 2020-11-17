1b70c2ed59c9173fc8ff7a690f5a2944e590f295
Bolivar <webdevelopmentbolivar@gmail.com>
2020-11-17 17:43:26 -0300

    erase comments

--- packages/api/src/modules/role/constants.ts
+++ packages/api/src/modules/role/constants.ts
@@ -6,8 +6,8 @@ export const ROLES = {
   OWNER: 'owner',
 };
 
-// Arrey ordenado Hierarquicamente
-export const ROLES_LIST = [ // mudar nome
+// Array ordenado Hierarquicamente
+export const ROLES_LIST = [
   ROLES.OWNER,
   ROLES.MANAGER,
   ROLES.DEVELOPER,
