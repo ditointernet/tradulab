542960c10dc435da9b4763a2a2e930cb61d27fc5
Uriell <uriellarttusviana+github@gmail.com>
2020-11-17 18:33:23 -0300

    file size limit from content length header

--- packages/api/src/modules/file/model.ts
+++ packages/api/src/modules/file/model.ts
@@ -10,8 +10,11 @@ import * as mongoose from 'mongoose';
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> Criado o module files e a resolver create File
+=======
+>>>>>>> file size limit from content length header
 import { IProject } from '../project/model';
 =======
 >>>>>>> Criado o module files e a resolver create File
@@ -43,7 +46,13 @@ import { IProject } from '../project/model';
 =======
 =======
 >>>>>>> Criado o module files e a resolver create File
+<<<<<<< HEAD
 >>>>>>> Criado o module files e a resolver create File
+=======
+=======
+
+>>>>>>> file size limit from content length header
+>>>>>>> file size limit from content length header
 import { EXTENSION_LIST } from './constants';
 >>>>>>> Criado o module files e a resolver create File
 
@@ -268,8 +277,12 @@ const schema = new mongoose.Schema(
     sourceLanguage: {
 >>>>>>> Create file resolver working at front-end and back-end without error treatment
       type: String,
+<<<<<<< HEAD
       required: true
 >>>>>>> Criado o module files e a resolver create File
+=======
+      required: true,
+>>>>>>> file size limit from content length header
     },
     project: {
       type: Types.ObjectId,
@@ -488,5 +501,11 @@ export default model;
 const model = mongoose.model('file', schema);
 
 export default model;
+<<<<<<< HEAD
 >>>>>>> Criado o module files e a resolver create File
+<<<<<<< HEAD
 >>>>>>> Criado o module files e a resolver create File
+=======
+=======
+>>>>>>> file size limit from content length header
+>>>>>>> file size limit from content length header
--- packages/api/src/modules/file/resolvers.ts
+++ packages/api/src/modules/file/resolvers.ts
@@ -14,11 +14,14 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 =======
 >>>>>>> Criado o module files e a resolver create File
 =======
 >>>>>>> formatting changes and some typings
+=======
+>>>>>>> file size limit from content length header
 import { ApolloError } from 'apollo-server-express';
 >>>>>>> Add Apollo Erros, fix merge conflicts, removing comments
 =======
@@ -358,60 +361,6 @@ export const queries = { listFiles };
 =======
   if (!project) {
     throw new ApolloError('The provided project does not exist.', 'PROJECT_NOT_FOUND');
-  }
->>>>>>> Corrigido erro de cors pra qualquer request
-
-  const file = new File({
-    filename,
-    translationProgress: 0,
-    approvalProgress: 0,
-    sourceLanguage: args.sourceLanguage,
-    extension: filename.split('.').pop(),
-=======
-=======
-import path from 'path';
-import fs from 'fs';
->>>>>>> Create file resolver working at front-end and back-end without error treatment
-import { model as File } from '.'
-=======
-import { FileUpload } from 'graphql-upload';
-
-import { model as File } from '.';
->>>>>>> formatting changes and some typings
-import { model as Project } from '../project';
-
-interface ICreateFileArgs {
-  file: FileUpload;
-  projectId: string;
-  sourceLanguage: string;
-}
-
-async function createFile(parent, args: ICreateFileArgs, context) {
-  const { createReadStream, filename } = await args.file;
-
-  // const stream = createReadStream();
-  // stream.on('data', (chunk) => console.log(chunk.toString()));
-  const project = await Project.findOne({ _id: args.projectId });
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-  const file = new File({
-    filename: args.filename,
-    translation_progress: 0,
-    approval_progress: 0,
-    source_language: args.source_language,
-    extension: args.extension,
->>>>>>> Criado o module files e a resolver create File
-    project,
-  });
-=======
-  // if (!project) {
-  //   throw new Error('The provided project does not exist.');
-  // }
->>>>>>> Create file resolver working at front-end and back-end without error treatment
-=======
-  if (!project) {
-    throw new Error('The provided project does not exist.');
   }
 >>>>>>> Corrigido erro de cors pra qualquer request
 
