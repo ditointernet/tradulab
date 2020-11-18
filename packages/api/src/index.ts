bd067dad2e191951370814fc8fd3a353b2cd9d40
Bolivar <webdevelopmentbolivar@gmail.com>
2020-11-18 19:07:29 -0300

    change aspas

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -14,7 +14,7 @@ app.use(function (err, req, res, next) {
   if (err.name === 'UnauthorizedError') {
     return res.status(401).json({ error: 'JWT Expired.' });
   }
-  console.error("Error: ", err.name)
+  console.error('Error: ', err.name)
 });
 
 function start() {
