a8c055a64d1f8943bc4b6789689c35b59632c680
Julio Cezar <jctaraujo@hotmail.com>
2020-11-23 16:17:32 -0300

    Add Apollo Erros, fix merge conflicts, removing comments

--- packages/api/src/modules/file/model.ts
+++ packages/api/src/modules/file/model.ts
@@ -8,6 +8,7 @@ import * as mongoose from 'mongoose';
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 =======
@@ -26,6 +27,8 @@ import * as mongoose from 'mongoose';
 >>>>>>> Criado o module files e a resolver create File
 =======
 >>>>>>> file size limit from content length header
+=======
+>>>>>>> Add Apollo Erros, fix merge conflicts, removing comments
 <<<<<<< HEAD
 import { IProject } from '../project/model';
 =======
@@ -40,6 +43,9 @@ import { IProject } from '../project/model';
 >>>>>>> Criado o module files e a resolver create File
 >>>>>>> Criado o module files e a resolver create File
 =======
+=======
+import { IProject } from '../project/model';
+>>>>>>> Add Apollo Erros, fix merge conflicts, removing comments
 =======
 import { IProject } from '../project/model';
 >>>>>>> Add Apollo Erros, fix merge conflicts, removing comments
@@ -325,6 +331,7 @@ const schema = new mongoose.Schema(
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> Add Apollo Erros, fix merge conflicts, removing comments
 export interface IFile extends mongoose.Document {
@@ -415,6 +422,21 @@ export default model;
 =======
 >>>>>>> Corrigido erro de cors pra qualquer request
 const model = mongoose.model('file', schema);
+=======
+export interface IFile extends mongoose.Document {
+  filename: string;
+  translationProgress: number;
+  approvalProgress: number;
+  project: mongoose.Types.ObjectId | IProject;
+  sourceLanguage: string;
+  extension: string;
+  filePath: string;
+  createdAt: Date;
+  updateAt: Date;
+};
+
+const model = mongoose.model<IFile>('file', schema);
+>>>>>>> Add Apollo Erros, fix merge conflicts, removing comments
 
 export default model;
 <<<<<<< HEAD
--- packages/api/src/modules/file/resolvers.ts
+++ packages/api/src/modules/file/resolvers.ts
@@ -10,6 +10,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { ApolloError } from 'apollo-server-express';
 =======
 <<<<<<< HEAD
@@ -42,6 +43,12 @@ import { ApolloError } from 'apollo-server-express';
 =======
 >>>>>>> formatting changes and some typings
 <<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+import { ApolloError } from 'apollo-server-express';
+>>>>>>> Add Apollo Erros, fix merge conflicts, removing comments
+>>>>>>> Add Apollo Erros, fix merge conflicts, removing comments
 =======
 import { ApolloError } from 'apollo-server-express';
 >>>>>>> Add Apollo Erros, fix merge conflicts, removing comments
@@ -184,11 +191,8 @@ async function createFile(parent, args: ICreateFileArgs, context) {
   const { createReadStream, filename } = await args.file;
 
   if (context.contentLength > MAX_ALLOWED_FILE_SIZE_IN_BYTES) {
-    throw new Error('File size exceeded, limit is 5MB.');
+    throw new ApolloError('File size exceeded, limit is 5MB.');
   }
-
-  // const stream = createReadStream();
-  // stream.on('data', (chunk) => console.log(chunk.toString()));
 
   const project = await Project.findOne({ _id: args.projectId });
 
@@ -444,7 +448,7 @@ export const queries = { listFiles };
   // }
 =======
   if (!project) {
-    throw new Error('The provided project does not exist.');
+    throw new ApolloError('The provided project does not exist.', 'PROJECT_NOT_FOUND');
   }
 >>>>>>> Corrigido erro de cors pra qualquer request
 
