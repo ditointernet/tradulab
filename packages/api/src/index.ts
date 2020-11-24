65c06742286bf0dba6dcc2d2084e5c9ccff4cec7
Julio Cezar <jctaraujo@hotmail.com>
2020-11-24 16:23:41 -0300

    Unnecessary comments and logs removed, add ApolloErrors

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,21 +2,12 @@ import * as express from 'express';
 import { mongo } from './config';
 import * as middlewares from './middlewares';
 import { env } from './helpers';
-<<<<<<< HEAD
-<<<<<<< HEAD
 // import { graphqlUploadExpress } from 'graphql-upload';
-
-const app = express();
-=======
-import { graphqlUploadExpress } from 'graphql-upload';
-=======
-// import { graphqlUploadExpress } from 'graphql-upload';
->>>>>>> formatting changes and some typings
 
 const app = express();
 
 // app.use(graphqlUploadExpress({ maxFileSize: 100, maxFiles: 1 }));
->>>>>>> Corrigido erro de cors pra qualquer request
+
 app.locals.mongo = mongo;
 
 app.use(middlewares.jwt);
@@ -24,15 +15,6 @@ app.use(middlewares.jwt);
 middlewares.apollo(app);
 
 app.use(function (err, req, res, next) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  console.log('ERROOOO', err);
-=======
-  console.log('ERROOOO', err)
->>>>>>> Corrigido erro de cors pra qualquer request
-=======
-  console.log('ERROOOO', err);
->>>>>>> formatting changes and some typings
   if (err.name === 'UnauthorizedError') {
     return res.status(401).json({ error: 'JWT Expired.' });
   }
