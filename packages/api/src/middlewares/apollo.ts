04141c2b93dcd92b8d596d91206b81ba3100ae60
Julio Cezar <jctaraujo@hotmail.com>
2020-11-26 15:09:18 -0300

    permission para permissions

--- packages/api/src/middlewares/apollo.ts
+++ packages/api/src/middlewares/apollo.ts
@@ -493,7 +493,7 @@ const resolvers = buildFederatedSchema([
   },
 ]);
 
-const permission = shield(
+const permissions = shield(
   {
     Query: {
       login: not(isAuthenticated, new ApolloError('Someone is already logged in.', 'ALREADY_LOGGED_IN')),
@@ -812,8 +812,12 @@ export default function ApolloMiddleware(app) {
 >>>>>>> permission para permissions
 =======
       resolvers,
+<<<<<<< HEAD
       permission,      
 >>>>>>> Organizando o apollo middleware
+=======
+      permissions,      
+>>>>>>> permission para permissions
     ),
 <<<<<<< HEAD
 =======
