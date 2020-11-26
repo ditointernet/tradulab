3fe191573e0786f39c7524a853838b96f5105fa4
Julio Cezar <jctaraujo@hotmail.com>
2020-11-26 15:48:25 -0300

    Rebase and resolving conflicts with master

--- packages/api/src/modules/file/model.ts
+++ packages/api/src/modules/file/model.ts
@@ -1,13 +1,6 @@
 import * as mongoose from 'mongoose';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { IProject } from '../project/model';
 
-=======
->>>>>>> Criado o module files e a resolver create File
-=======
-
->>>>>>> file size limit from content length header
 import { EXTENSION_LIST } from './constants';
 
 const { Types } = mongoose.Schema;
@@ -19,51 +12,21 @@ const schema = new mongoose.Schema(
       index: true,
       required: true,
     },
-<<<<<<< HEAD
-<<<<<<< HEAD
     translationProgress: {
-=======
-    translation_progress: {
->>>>>>> Criado o module files e a resolver create File
-=======
-    translationProgress: {
->>>>>>> Create file resolver working at front-end and back-end without error treatment
       type: Number,
       min: [0, 'Out of range'],
       max: [1, 'Out of range'],
       required: true,
     },
-<<<<<<< HEAD
-<<<<<<< HEAD
     approvalProgress: {
-=======
-    approval_progress: {
->>>>>>> Criado o module files e a resolver create File
-=======
-    approvalProgress: {
->>>>>>> Create file resolver working at front-end and back-end without error treatment
       type: Number,
       min: [0, 'Out of range'],
       max: [1, 'Out of range'],
       required: true,
     },
-<<<<<<< HEAD
-<<<<<<< HEAD
     sourceLanguage: {
       type: String,
       required: true,
-=======
-    source_language: {
-=======
-    sourceLanguage: {
->>>>>>> Create file resolver working at front-end and back-end without error treatment
-      type: String,
-<<<<<<< HEAD
-      required: true
->>>>>>> Criado o module files e a resolver create File
-=======
-      required: true,
->>>>>>> file size limit from content length header
     },
     project: {
       type: Types.ObjectId,
@@ -76,22 +39,9 @@ const schema = new mongoose.Schema(
       required: true,
       enum: EXTENSION_LIST,
     },
-<<<<<<< HEAD
-<<<<<<< HEAD
     filePath: {
       type: String,
     },
-=======
-    file_path: {
-      type: String,
-      required:true,
-    }
->>>>>>> Criado o module files e a resolver create File
-=======
-    filePath: {
-      type: String,
-    },
->>>>>>> Create file resolver working at front-end and back-end without error treatment
   },
   {
     timestamps: true,
@@ -99,8 +49,6 @@ const schema = new mongoose.Schema(
   }
 );
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 export interface IFile extends mongoose.Document {
   filename: string;
   translationProgress: number;
@@ -116,25 +64,3 @@ export interface IFile extends mongoose.Document {
 const model = mongoose.model<IFile>('file', schema);
 
 export default model;
-=======
-=======
-// export interface IFile extends mongoose.Document {
-//   filename: string;
-//   translationProgress: number;
-//   approvalProgress: number;
-//   project: mongoose.Types.ObjectId | IProject;
-//   sourceLanguage: string;
-//   extension: string;
-//   filePath: string;
-//   createdAt: Date;
-//   updateAt: Date;
-// }
-
->>>>>>> Corrigido erro de cors pra qualquer request
-const model = mongoose.model('file', schema);
-
-export default model;
-<<<<<<< HEAD
->>>>>>> Criado o module files e a resolver create File
-=======
->>>>>>> file size limit from content length header
