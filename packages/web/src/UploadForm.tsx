8d6984ae0593bfbba79534ad9716936d2fbae317
Julio Cezar <jctaraujo@hotmail.com>
2020-11-26 15:02:33 -0300

    removido transpile only ts-node-dev, engine node 12

--- packages/web/src/UploadForm.tsx
+++ packages/web/src/UploadForm.tsx
@@ -287,6 +287,7 @@ export const UPLOAD_FILE = gql`
 `;
 
 export default function UploadForm() {
+  // Trocar email e password para o que estiver no banco de dados local
   const { data: dataLogin, error } = useQuery(LOGIN, {
     variables: { email: "julinho2801@gmail.com", password: "123456" },
   });
@@ -305,7 +306,7 @@ export default function UploadForm() {
 >>>>>>> Create file resolver working at front-end and back-end without error treatment
 =======
     const file = e.target.files![0];
-    const projectId = "5fb52bfe99f0a22dc58d206b";
+    const projectId = "5fb52bfe99f0a22dc58d206b"; // Trocar id do projeto para o que estiver no banco de dados local
     const sourceLanguage = "PT-BR";
     if (!file) return;
     createFile({ variables: { file, projectId, sourceLanguage } });
