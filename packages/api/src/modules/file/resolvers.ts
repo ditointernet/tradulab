fe0484ec2d3a4c3f82b2d411f01288a674fdbd50
Bolivar <webdevelopmentbolivar@gmail.com>
2020-11-30 10:26:09 -0300

    changes

--- packages/api/src/modules/file/resolvers.ts
+++ packages/api/src/modules/file/resolvers.ts
@@ -339,7 +339,7 @@ interface IListFileArgs {
 
 async function listFiles(_, args: IListFileArgs, context) {
   const { projectId } = args;
-  console.log('projectId', projectId);
+
   const role = Role.findOne({ user: context.user.id, project: projectId });
 
   if (!role) {
