affe698eb0c65299cdf6464398042eea9370902a
Bolivar <webdevelopmentbolivar@gmail.com>
2020-12-03 12:56:23 -0300

    update listFiles and error

--- packages/api/src/modules/file/constants.ts
+++ packages/api/src/modules/file/constants.ts
@@ -7,9 +7,28 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+export const ERROR_CODES = {
+  MAX_ALLOWED_FILE_SIZE: 'MAX_ALLOWED_FILE_SIZE',
+  NOT_A_MEMBER: 'NOT_A_MEMBER',
+  PROJECT_NOT_FOUND: 'PROJECT_NOT_FOUND',
+};
+
+export const ERROR_MESSAGES = {
+  MAX_ALLOWED_FILE_SIZE: 'File size exceeded, limit is 5MB.',
+  NOT_A_MEMBER: "You don't have a role in this project",
+  PROJECT_NOT_FOUND: 'The provided project does not exist.',
+};
+
+export const INTERNAL_ERROR = 'INTERNAL_ERROR';
+
+>>>>>>> update listFiles and error
 export const EXTENSION_LIST = ['json', 'txt', 'csv'];
+
 export const MAX_ALLOWED_FILE_SIZE_IN_BYTES = 1e3 * 1e3 * 5; // currently 5MB
 
+<<<<<<< HEAD
 export const ERROR_CODES = {
   SIZE_EXCEEDED: 'SIZE_EXCEEDED',
 };
@@ -113,3 +132,8 @@ export const MAX_ALLOWED_FILE_SIZE_IN_BYTES = 1e3 * 1e3 * 5; // currently 5MB
 export const EXTENSION_LIST = ['json', 'txt', 'csv'];
 export const MAX_ALLOWED_FILE_SIZE_IN_BYTES = 1e3 * 1e3 * 5; // currently 5MB
 >>>>>>> file size limit from content length header
+=======
+export const REGEXES = {
+  SLUG: /^[a-z0-9-]{3,}$/,
+};
+>>>>>>> update listFiles and error
