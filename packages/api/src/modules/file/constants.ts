4df7c7e7ac8d185fbbb11b4b8d21523f89524b43
Julio Cezar <jctaraujo@hotmail.com>
2020-12-04 16:41:53 -0300

    created constants, index and model within the file module

---
+++ packages/api/src/modules/file/constants.ts
@@ -0,0 +1,10 @@
+export const EXTENSION_LIST = ['json', 'txt', 'csv'];
+export const MAX_ALLOWED_FILE_SIZE_IN_BYTES = 1e3 * 1e3 * 5; // currently 5MB
+
+export const ERROR_CODES = {
+  SIZE_EXCEEDED: 'SIZE_EXCEEDED',
+};
+
+export const ERROR_MESSAGES = {
+  SIZE_EXCEEDED: 'File size exceeded, limit is 5MB.',
+};
