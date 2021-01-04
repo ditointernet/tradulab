84249488553e61a683015df55f66376f6f131e28
Julio Cezar <jctaraujo@hotmail.com>
2021-01-04 09:50:54 -0300

    Review

--- packages/web/src/ProgressBar.tsx
+++ packages/web/src/ProgressBar.tsx
@@ -21,16 +21,18 @@ const useStyles = makeStyles((theme: Theme) =>
 interface IProgressBarProps {
   approvalPercentage: number,
   translatePercentage: number,
+  containerWidth: string;
 }
 
 const ProgressBar: React.FC<IProgressBarProps> = ({
   approvalPercentage,
   translatePercentage,
+  containerWidth,
 }) => {
   const classes = useStyles();
 
   return (
-    <Box className={classes.root} width="100%">
+    <Box className={classes.root} width={containerWidth || "100%"}>
       <Box className={classes.approval} width={`${approvalPercentage}%`} />
       <Box className={classes.translate} width={`${translatePercentage - approvalPercentage}%`} />
     </Box>
