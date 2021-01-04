da285a8fc833a7ac2af09130f97de1dd6c7a9d82
Julio Cezar <jctaraujo@hotmail.com>
2021-01-04 12:24:22 -0300

    Created open menu and fix reviews

--- packages/web/src/components/NavBar.tsx
+++ packages/web/src/components/NavBar.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
-import { AppBar, Toolbar, Typography, Button, makeStyles, Theme, createStyles } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  makeStyles,
+  Theme,
+  createStyles,
+  Menu,
+  MenuItem
+} from '@material-ui/core';
 import { AccountCircle, ArrowDropDown } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -13,18 +23,32 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     username: {
       margin: theme.spacing(0, 1, 0, 1)
+    },
+    menu: {
+      "& .MuiPaper-root": {
+        backgroundColor: '#59AB6C',
+        color: 'white',
+      }
     }
   })
 );
 
 interface INavBarProps {
   username: string;
-  onClick: () => void;
+  handleMenu: (event: React.MouseEvent<HTMLElement>) => void;
+  menuAnchorEl: HTMLElement | null;
+  openMenu: boolean;
+  handleClick: (event: any) => void;
+  handleCloseMenu: () => void;
 }
 
 const NavBar: React.FC<INavBarProps> = ({
   username,
-  onClick,
+  handleMenu,
+  menuAnchorEl,
+  openMenu,
+  handleClick,
+  handleCloseMenu,
 }) => {
   const classes = useStyles();
 
@@ -34,13 +58,40 @@ const NavBar: React.FC<INavBarProps> = ({
         <Typography variant="h6" className={classes.title}>
           Tradulab
         </Typography>
-        <Button color="inherit" onClick={onClick}>
+        <Button color="inherit" onClick={handleMenu}>
           <AccountCircle />
           <Typography variant="body2" className={classes.username}>
             {username}
           </Typography>
           <ArrowDropDown />
         </Button>
+        <Menu
+          className={classes.menu}
+          id="menu-appbar"
+          anchorEl={menuAnchorEl}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={openMenu}
+          onClose={handleCloseMenu}
+        >
+          <MenuItem onClick={handleClick}>
+            <Typography id="config" variant="body1">
+              Configurações
+            </Typography>
+          </MenuItem>
+          <MenuItem onClick={handleClick}>
+            <Typography id="logout" variant="body1">
+              Sair
+            </Typography>
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
