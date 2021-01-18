fc12d6df34e9ccb7604a52f8e3cbd20bbd760b88
Miguel Miranda <miguelmirandaazevedo@gmail.com>
2021-01-17 22:50:27 -0300

    Definição do type Suggestion

--- packages/api/src/modules/suggestion/types.ts
+++ packages/api/src/modules/suggestion/types.ts
@@ -1,3 +1,25 @@
 import { gql } from 'apollo-server-express';
 
-export default gql``;
+export default gql`
+  enum Lang {
+    pt-br
+    en-us
+  }
+
+  # É uma boa ideia fazer o rating assim?
+  type Rating {
+    votes: [ID!]!
+    score: Number!
+  }
+
+  type Suggestion {
+    _id: ID!
+    phrase: ID!
+    user: ID!
+    lang: Lang!
+    text: String!
+    rating: Rating!
+    createdAt: Date!
+    updatedAt: Date!
+  }
+`;
