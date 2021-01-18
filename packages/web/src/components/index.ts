78471464862406218683617344951686e7420c9b
Uriell <uriellarttusviana+github@gmail.com>
2021-01-18 15:50:21 -0300

    Criando novos componentes e adicionando a dependência
    Co-authored-by: Bolivar <webdevelopmentbolivar@gmail.com>

--- packages/web/src/components/index.ts
+++ packages/web/src/components/index.ts
@@ -1,4 +1,4 @@
+export { default as Loading } from './Loading';
 export { default as ProfileCard } from './ProfileCard';
 export { default as ProjectsTable } from './ProjectsTable';
-// export { default as TradulabHeader } from './TradulabHeader';
-// export { default as Loading } from './Loading';
+export { default as TradulabHeader } from './TradulabHeader';
