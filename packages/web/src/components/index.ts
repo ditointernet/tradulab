8cd87114e05f19622632e6d77943b1691810aa65
Bolivar <webdevelopmentbolivar@gmail.com>
2021-01-18 18:44:29 -0300

    Add Background Image and Increase Login Form Height

--- packages/web/src/components/index.ts
+++ packages/web/src/components/index.ts
@@ -2,4 +2,5 @@ export { default as TradulabHeader } from './TradulabHeader';
 export { default as Dashboard } from './Dashboard';
 export { default as LoginForm } from './LoginForm';
 export { default as Loading } from './Loading';
+export { default as ProfileCard } from './ProfileCard';
 export { default as TradulabTitle } from './TradulabTitle';
