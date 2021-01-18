e21641b9c5b8b5b7642f1b5ae1bc8c475434a48e
Uriell <uriellarttusviana+github@gmail.com>
2021-01-18 15:28:16 -0300

    Criar o service do Apollo e remover uns arquivos
    Co-authored-by: Bolivar <webdevelopmentbolivar@gmail.com>

---
+++ packages/web/src/services/ApolloClient.ts
@@ -0,0 +1,34 @@
+import {
+  ApolloClient,
+  ApolloLink,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+  credentials: 'include',
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem('token');
+
+  // console.log('operation', operation);
+
+  if (!['loginUser', 'createUser'].includes(operation.operationName)) {
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  }
+
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+});
+
+export default client;
