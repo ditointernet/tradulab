371a465bb43516bbbe23f731b8e7b149c51c9162
Uriell <uriellarttusviana+github@gmail.com>
2021-01-18 17:11:20 -0300

    Adicionando o Router no App
    Co-authored-by: Bolivar <webdevelopmentbolivar@gmail.com>

--- packages/web/src/containers/index.ts
+++ packages/web/src/containers/index.ts
@@ -1,3 +1,4 @@
 export { default as TradulabLayout } from './TradulabLayout';
 export { default as NavBarContainer } from './NavBarContainer';
 export { default as ProfileCardContainer } from './ProfileCardContainer';
+export { default as Router } from './Router';
