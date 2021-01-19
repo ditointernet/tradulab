d4183da1a54467b12da225bc15900f2ce37333f1
Julio Cezar <jctaraujo@hotmail.com>
2021-01-19 17:04:51 -0300

    Add new file constant

--- packages/api/src/modules/file/constants.ts
+++ packages/api/src/modules/file/constants.ts
@@ -2,16 +2,18 @@ export const ERROR_CODES = {
   MAX_ALLOWED_FILE_SIZE: 'MAX_ALLOWED_FILE_SIZE',
   NOT_A_MEMBER: 'NOT_A_MEMBER',
   PROJECT_NOT_FOUND: 'PROJECT_NOT_FOUND',
-  FILE_DOESNT_EXIST: 'FILE_DOESNT_EXIST',
+  FILE_NOT_FOUND: 'FILE_NOT_FOUND',
   NOT_ALLOWED: 'NOT_ALLOWED',
+  FILE_SIZE_EXCEED: 'FILE_SIZE_EXCEED',
 };
 
 export const ERROR_MESSAGES = {
   MAX_ALLOWED_FILE_SIZE: 'File size exceeded, limit is 5MB.',
   NOT_A_MEMBER: "You don't have a role in this project",
   PROJECT_NOT_FOUND: 'The provided project does not exist.',
-  FILE_DOESNT_EXIST: 'The provided file does not exist.',
+  FILE_NOT_FOUND: 'The provided file does not exist.',
   NOT_ALLOWED: "You don't have permission to execute this action.",
+  FILE_SIZE_EXCEED: "File size exceeded, limit is 5MB."
 };
 
 export const INTERNAL_ERROR = 'INTERNAL_ERROR';
