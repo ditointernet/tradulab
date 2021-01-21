245fbc44439bdd80284dae5f1aaee28a8f61b6bc
Miguel Miranda <miguelmirandaazevedo@gmail.com>
2021-01-21 16:16:14 -0300

    Adicionando rating e mensagens de erro

--- packages/api/src/modules/suggestion/model.ts
+++ packages/api/src/modules/suggestion/model.ts
@@ -1,14 +1,19 @@
 import * as mongoose from 'mongoose';
 
-import { LANGS_LIST } from './constants';
+import { LANGS_LIST, ERROR_CODES } from './constants';
 
 const { Types } = mongoose.Schema;
 
-const schema = new mongoose.Schema(
+const ratingSchema = new mongoose.Schema({
+  positiveVotes: [Types.ObjectId],
+  negativeVotes: [Types.ObjectId],
+});
+
+const suggestionSchema = new mongoose.Schema(
   {
     phrase: {
       type: Types.ObjectId,
-      ref: 'phrase', // O código quebra se eu referenciar um model que ainda não existe?
+      ref: 'phrase',
       required: true,
       index: true,
     },
@@ -27,12 +32,12 @@ const schema = new mongoose.Schema(
     text: {
       type: String,
       required: true,
-      // Faz sentido ter erro de tamanho da string aqui? Se sim, que tamanhos máximo e
-      // mínimo seriam esses, dado que uma "frase" pode ser uma palavra só ou até um texto?
+      minlength: [1, ERROR_CODES.TEXT_SHORT],
+      maxlength: [10000, ERROR_CODES.TEXT_LONG],
     },
     rating: {
-      // Preciso pesquisar como tipar um "objeto" no mongoose, se realmente formos deixar
-      // o rating daquela forma. Se alguém souber já me fala.
+      type: ratingSchema,
+      required: true,
     },
   },
   {
@@ -41,6 +46,6 @@ const schema = new mongoose.Schema(
   }
 );
 
-const model = mongoose.model('suggestion', schema);
+const model = mongoose.model('suggestion', suggestionSchema);
 
 export default model;
