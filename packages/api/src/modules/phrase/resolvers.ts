babc2bf7aeba90a100748653a85712bf4a154cff
Bolivar <webdevelopmentbolivar@gmail.com>
2021-01-25 21:10:08 -0300

    Add Create Phrase

---
+++ packages/api/src/modules/phrase/resolvers.ts
@@ -0,0 +1,36 @@
+import { ApolloError } from 'apollo-server-express';
+
+import { ERROR_CODES } from './constants';
+import { model as File } from '../file';
+import { model as Phrase } from '.';
+import TradulabError from '../../errors';
+
+interface ICreatePhraseArgs {
+  fileId: string;
+  text: string;
+}
+
+async function createPhrase(_, args: ICreatePhraseArgs) {
+  const { fileId, text } = args;
+
+  const file = await File.findById(fileId);
+
+  if (!file) throw new TradulabError(ERROR_CODES.FILE_NOT_EXIST);
+
+  const phrase = new Phrase({
+    file: fileId,
+    text,
+  });
+
+  try {
+    await phrase.save();
+
+    return phrase;
+  } catch (err) {
+    console.error(err);
+
+    throw new ApolloError(err.message);
+  }
+}
+
+export const mutations = { createPhrase };
