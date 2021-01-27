1964a13d680acd69fd177505c446af38af768e9a
Bolivar <webdevelopmentbolivar@gmail.com>
2021-01-27 08:58:22 -0300

    QA fix

--- packages/api/src/modules/suggestion/constants.ts
+++ packages/api/src/modules/suggestion/constants.ts
@@ -2,6 +2,7 @@ export const ERROR_CODES = {
   TEXT_SHORT: 'TEXT_SHORT',
   TEXT_LONG: 'TEXT_LONG',
 };
+
 export const ERROR_MESSAGES = {
   TEXT_SHORT: 'The suggestion text is too short.',
   TEXT_LONG: 'The suggestion text is too long.',
--- packages/api/src/modules/suggestion/types.ts
+++ packages/api/src/modules/suggestion/types.ts
@@ -2,8 +2,8 @@ import { gql } from 'apollo-server-express';
 
 export default gql`
   enum Lang {
-    PT_BR
-    EN_US
+    pt_br
+    en_us
   }
 
   type Rating {
